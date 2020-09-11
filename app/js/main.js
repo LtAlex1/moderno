c60b2fa7db402d065e9574ab28f78d32aa0f1939
John Doe <johndoe@example.com>
2020-09-11 20:56:46 +0300

    add product slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
-$(function() {
+
+$(function(){
+    $('.product-slider__inner').slick({
+        dots:true,
+        arrows:false,
+        slidesToShow:4,
+        slidesToScroll:4,
+    });
 
     let mixer =mixitup('.products__inner-box')
 })
+
