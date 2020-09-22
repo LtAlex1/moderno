dfb5d6e72fc216b2e7ef340de99c62969b7f7d24
John Doe <johndoe@example.com>
2020-09-22 03:17:52 +0300

    Added search line

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,14 @@ $(function(){
         slidesToScroll:4,
     });
 
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+    })
     let mixer =mixitup('.products__inner-box')
 })
 
